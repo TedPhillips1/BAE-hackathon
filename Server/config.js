43581701af9c13c8dbd559f040ec494932d84b38
Julija Stepanova <s.julija@me.com>
2022-03-28 12:06:13 +0100

    heroku credentials added

---
+++ Server/config.js
@@ -0,0 +1,7 @@
+export const password = process.env.PASSWORD
+export const user = process.env.USER
+export const DBPort = process.env.PORT
+export const theDB = process.env.DATABASE
+export const host = process.env.HOST
+
+
